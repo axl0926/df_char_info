3f2fcfb8bfa0c0f75b380cac12ee7c04420ee302
axl0926 <song22092000@gmail.com>
2023-01-27 08:26:45 +0900

    feat:아이템 분류에 따른 테두리 추가

--- components/Loading.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-import { ItemSlot } from "../types/Types";
-import Image from "next/image";
-
-export default function Loading(isFirstSearch: boolean) {
-    return <>로딩중</>;
-}

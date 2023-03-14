3d9ce2be32040aba7d0f79132bf1d1c508998b36
W9 <70824589+axl0926@users.noreply.github.com>
2023-03-14 17:11:51 +0900

    Update neople.ts

--- lib/neople.ts
+++ lib/neople.ts
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
     baseURL: 'api'
-=======
-    baseURL: '/api'
->>>>>>> 8f02cf63c91f0614b4b7f7f3f61ce0bf6eddcfde
 });
 const getApi = {
     getCharInfo: async (serverId: string, characterName: string) => (await api.get(`/servers/${serverId}/characters?characterName=${characterName}`)).data,

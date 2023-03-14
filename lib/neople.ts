ae1798ae4e9cd3361993451193bec8409ff4ecd7
W9 <70824589+axl0926@users.noreply.github.com>
2023-03-14 17:23:46 +0900

    Update neople.ts

--- lib/neople.ts
+++ lib/neople.ts
@@ -4,10 +4,10 @@ const api = axios.create({
   baseURL: process.env.NODE_ENV === 'production' ? `${window.location.protocol}//${window.location.host}/api` : '/api'
 });
 const getApi = {
-    getCharInfo: async (serverId: string, characterName: string) => (await api.get(`/api/servers/${serverId}/characters?characterName=${characterName}`)).data,
-    getCharStatus: async (serverId: string, characterId: string) => (await api.get(`/api/servers/${serverId}/characters/${characterId}/status`)).data,
-    getEquipInfo: async (serverId: string, characterId: string) => (await api.get(`/api/servers/${serverId}/characters/${characterId}/equip/equipment`)).data,
-    getItemInfo: async (itemId: string) => (await api.get(`/api/items/${itemId}`)).data,
+    getCharInfo: async (serverId: string, characterName: string) => (await api.get(`/servers/${serverId}/characters?characterName=${characterName}`)).data,
+    getCharStatus: async (serverId: string, characterId: string) => (await api.get(`/servers/${serverId}/characters/${characterId}/status`)).data,
+    getEquipInfo: async (serverId: string, characterId: string) => (await api.get(`/servers/${serverId}/characters/${characterId}/equip/equipment`)).data,
+    getItemInfo: async (itemId: string) => (await api.get(`/items/${itemId}`)).data,
 };
 
 export default getApi;

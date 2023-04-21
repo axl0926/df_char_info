45454c169885e1c6112661df9877b3087001d05f
axl0926 <song22092000@gmail.com>
2023-04-22 03:06:05 +0900

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,6 @@
 import React, { Suspense, useState } from "react";
-import {ErrorBoundary,FallbackProps} from 'react-error-boundary'
 import SelectServer from "../components/SelectServer";
 import SearchCharacterList from "../components/SearchCharacterList";
-
-function ErrorFallback({error, resetErrorBoundary}:FallbackProps) {
-    return (
-      <div >
-        <p>Error:</p>
-        <pre>{error.message}</pre>
-        <button onClick={resetErrorBoundary}>Try again</button>
-      </div>
-    )
-  }
 
 export default function Home() {
     const [serverId, setServerId] = useState("cain");
@@ -49,13 +38,7 @@ export default function Home() {
                 검색
             </button>
             <div id="char_info">
-                <Suspense fallback="loading...">
-                    {isFirstSearch && (
-                        <ErrorBoundary  FallbackComponent={ErrorFallback}>
-                            <SearchCharacterList selectedCharacter={selectedCharacter} />{" "}
-                        </ErrorBoundary>
-                    )}
-                </Suspense>
+                <Suspense fallback="loading...">{isFirstSearch && <SearchCharacterList selectedCharacter={selectedCharacter} />}</Suspense>
             </div>
         </div>
     );
